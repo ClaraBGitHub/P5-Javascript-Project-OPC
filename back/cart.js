e505247faf953c7e69d80465c0776208a8bbb8cf
Clara Jacono <clara.jacono@gmail.com>
2022-05-05 20:31:38 -0400

    cart delete item

--- back/cart.js
+++ back/cart.js
@@ -5,44 +5,130 @@ if (cart == null) {
     cart = JSON.parse(cart);
 } 
 var total = 0;
+var totalQuantity = 0;
 
-cart.forEach(element => { // Je boucle chaque element de mon cart
-    fetch ("http://localhost:3000/api/products/"+element._id) // Je récupère l'id de chaque élement de mon cart
-    .then (response => {
-          return response.json();
-      })
-    .then (kanape => { 
-        console.log(kanape)
-            template = document.getElementById("template");
-            node = template.cloneNode(true); //Je clone mon template dans "node"
-            // Image
-            node.querySelector('[id=cart__item__img]').src = kanape.imageUrl;
-            console.log(node.querySelector('[id=cart__item__img]').src)
-            // Name
-            var namesDisplay = node.querySelector('[id=nomDuProduit]');
-            namesDisplay.textContent = kanape.names;
-            console.log(namesDisplay)
-            // Color
-            var colorKanape = node.querySelector('[id=colorKanape]');
-            colorKanape.textContent = element.color;
-            console.log(colorKanape)
-            //Quantity
-            var quantityDisplay = node.querySelector('[class=itemQuantity]');
-            quantityDisplay.value = element.quantity;
-            console.log(quantityDisplay);
-            // Price
-            var priceDisplay = node.querySelector('[id=priceKanape]');
-            priceDisplay.textContent = kanape.price;
-            console.log(priceDisplay)
-            console.log(node.classList)
-            node.classList.remove("hidden");
-            // console.log(priceDisplay);
-            document.getElementById('cart__items').appendChild(node); //Le noeud est ajouté au document
+const promises = cart.map(cartElement => {
+    return fetch ("http://localhost:3000/api/products/"+cartElement._id).then(response => {
+        return response.json();
     })
-    .catch((err) => {
-    // Une erreur est survenue
-    });
 });
+Promise.all(promises).then(products => {
+    cart.forEach(element => { // Je boucle chaque element de mon cart
+        var kanape = null;
+        products.forEach(product => {
+            if (product._id == element._id){
+                kanape = product;
+            }
+        })
+        template = document.getElementById("template");
+        node = template.cloneNode(true); //Je clone mon template dans "node"
+        //data-id
+        template.dataset.id = element._id;
+        //data-color
+        template.dataset.color = element.color;
+        // Image
+        node.querySelector('[id=cart__item__img]').src = kanape.imageUrl;
+        // Name
+        var namesDisplay = node.querySelector('[id=nomDuProduit]');
+        namesDisplay.textContent = kanape.name;
+        // Color
+        var colorKanape = node.querySelector('[id=colorKanape]');
+        colorKanape.textContent = element.color;
+        //Quantity
+        var quantityDisplay = node.querySelector('[class=itemQuantity]');
+        quantityDisplay.value = parseInt(element.quantity);
+
+        // Price
+        var priceDisplay = node.querySelector('[id=priceKanape]');
+        priceDisplay.textContent = kanape.price;
+
+        node.classList.remove("hidden");
+        document.getElementById('cart__items').appendChild(node); //Le noeud est ajouté au document
+
+        // Final price
+        var priceElement = document.getElementById('totalPrice');
+        let productCost = kanape.price * parseInt(element.quantity);
+        total += productCost;
+        priceElement.textContent = total;
+
+        //Final product quantity
+        var quantityElement = document.getElementById('totalQuantity');
+        let finalQuantity = element.quantity;
+        totalQuantity += finalQuantity;
+        quantityElement.textContent = totalQuantity;
+        
+        deleteButton = node.querySelector('[class=deleteItem]');
+        deleteButton.addEventListener('click', (event) => {
+            newCart = [];
+            cart.forEach(elt => {
+               if (elt._id != element._id || elt.color != element.color) {
+                   newCart.push(elt);
+               }
+           })
+           localStorage.setItem("productsInCart", JSON.stringify(newCart));
+           location.reload();
+        });
+    })
+});
+
+
+// Cart EventListener --> Ajout d'une quantité à mon panier
+// var cartQuantity = document.querySelector('[class=itemQuantity]');
+// cartQuantity.forEach(change => {
+//         console.log(cartQuantity)
+//         console.log("test du changement");
+//         change.addEventListener('change', () => {
+//         if (cart._id == change.dataset.id && cart.color == change.dataset.color) {
+//             console.log(change.value)
+//             cart.quantity = parseInt(change.value),
+//             localStorage.setItem('productsInCart', JSON.stringify(cart));
+//         }
+//     })
+// });
+
+// 1- Je veux que lorsque j’ajoute une quantité depuis mon panier (addEvent), cela vient modifier mon local storage.
+// J’écouté le changement de quantité sur ma page panier
+// Je récupère cette quantité
+// Je la push dans mon local storage
+
+
+// Cart delete product 
+
+// var deleteButtons = document.getElementsByClassName('deleteItem');
+// Array.from(deleteButtons).forEach(button => {
+//     button.addEventListener('click', (id, color) => {
+//         alert('toto')
+//     });
+// })
+// deleteButton.addEventListener('click', (id, color) => {
+//     alert("toto")
+//     console.log(id, color)
+        // for (let i = 0; i < cart.length; i++) {
+        //     // console.log(id, color);
+        //     if (cart[i]._id == element.dataset.id && cart[i].color == element.dataset.color) {
+        //         cart.splice(i, 1);
+        //         console.log("supprimé");
+        //         return (
+        //             localStorage.removeItem("productsInCart"),
+        //             location.reload()
+        //         )
+        //     }
+        // }
+    // })
+
+
+// 2- Lorsque je clique sur le bouton « supprimer », cela supprime le Kanape de mon local storage et de ma page panier.
+// J’écouté le clique sur le bouton de ma page panier
+// Je supprime l’objet en question de mon local storage ET de ma page panier
+
+
+
+
+
+
+
+
+
 
 
 
